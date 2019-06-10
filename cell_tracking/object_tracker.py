46201bcd691c3b40243961ce4e9aba00b5e7b55d
pranathi.vemuri <pranathi93.vemuri@gmail.com>
2019-06-10 15:34:51 -0700

    separate contour stats and add cell distance, cell displacement to trajectory path graph

--- cell_tracking/object_tracker.py
+++ cell_tracking/object_tracker.py
@@ -14,6 +14,7 @@ import skimage.morphology
 
 from cell_tracking.centroid_tracker import CentroidTracker
 import cell_tracking.skeleton_graph_stats as skeleton_stats
+import cell_tracking.contour_stats as contour_features
 
 
 SKELETON_COLOR = [255, 0, 0]
@@ -27,70 +28,13 @@ def _get_objects(arr):
     return count_objects
 
 
-def get_centroid(contour):
-    moments = cv2.moments(contour)
-    if moments['m00'] != 0:
-        cx = int(moments['m10'] / moments['m00'])
-        cy = int(moments['m01'] / moments['m00'])
-    elif len(contour) == 2:
-        cx = (contour[0][0][0] + contour[1][0][0]) // 2
-        cy = (contour[0][0][1] + contour[1][0][1]) // 2
-    else:
-        cx, cy = contour[0][0][0], contour[0][0][1]
-    return tuple((cx, cy))
-
-
-def get_contour_stats(contour):
-    """
-    Add contour metrics as object attributes for given contour xy.
-    This is the primary wrapper for openCV contour attributes
-
-    Raises AssertionError if the last element of contour is not equal to the first
-    """
-    xy = contour
-
-    x, y, w, h = cv2.boundingRect(contour)
-
-    contour_stats = {}
-    filled_area = cv2.contourArea(xy)
-    contour_stats["filled_area"] = filled_area
-    centroid = get_centroid(xy)
-    contour_stats["centroid"] = centroid
-    contour_stats["aspect_ratio"] = float(w) / h
-    contour_stats["boxed_centroid"] = (centroid[0] - x, centroid[1] - y)
-    contour_stats["perimeter"] = cv2.arcLength(xy, True)
-    contour_stats["extent"] = float(filled_area) / (w * h)
-    convex_hull = cv2.convexHull(xy).squeeze(axis=1)
-    if cv2.contourArea(convex_hull) != 0:
-        contour_stats["solidity"] = filled_area / cv2.contourArea(convex_hull)
-    else:
-        contour_stats["solidity"] = 0
-
-    # remove the last coordinate we added to close the loop
-    contour_stats["is_convex"] = cv2.isContourConvex(xy[:-1])
-    contour_stats["equiv_diameter"] = np.sqrt(4 * filled_area / np.pi)
-
-    # Need at least 5 contour points for an ellipse
-    if contour.shape[0] >= 5:
-        ellipse = list(cv2.fitEllipse(contour))  # wrap so we return a (mutable) list
-        # rotate the angle clockwise by 90 deg to account for tranpose
-        ellipse[2] = np.mod(ellipse[2] + 90, 180)
-
-        # if the ellipse is a circle, orientation should be 0
-        if ellipse[1][0] == ellipse[1][1]:
-            ellipse[2] = 0
-    else:
-        ellipse = [(0, 0), (0, 0), 0]
-
-    contour_stats["minor_axis_diameter"] = ellipse[1][0]
-    contour_stats["major_axis_diameter"] = ellipse[1][1]
-
-    # angle between x-axis and the major axis of the matching ellipse
-    contour_stats["orientation"] = ellipse[2]
-    return contour_stats
-
-
 def get_aggregate_stats(stats):
+    """
+    Returns aggregate stss i.e reduces the stats per each branch in the skeleton to a list
+    so there is stats per whole skeleton in form of dictionary. if it has 5 branches has 5 lists for each fo the features
+    nodes, length, contraction, start_point, end_point
+    Ex: {'length': [1, 2], 'contraction': [1, 1.2], 'start_point':[(1, 2), (3, 4)], 'end_point': [(5, 6), (7, 8)]}
+    """
     expected_keys = 'nodes', 'length', 'contraction', 'start_point', 'end_point'
     reduced_segments = {key: [] for key in expected_keys}
     for segment in stats:
@@ -103,47 +47,73 @@ def get_aggregate_stats(stats):
     return reduced_segments
 
 
-def get_overlapped_skeleton_image(image, skeleton_image):
+def get_overlapped_skeleton_image(binary_image, skeleton_image):
     """
-    Returns overlay the 'image' with the skeleton in red
+    Returns overlay the 'binary_image' where with the skeleton is in SKELETON_COLOR and binary_image is in CELL_COLOR
     """
-    overlaid_image = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
-    overlaid_image[image != 0] = CELL_COLOR
+    overlaid_image = np.zeros((binary_image.shape[0], binary_image.shape[1], 3), dtype=np.uint8)
+    overlaid_image[binary_image != 0] = CELL_COLOR
 
     overlaid_image[skeleton_image != 0] = SKELETON_COLOR
     return cv2.cvtColor(overlaid_image, cv2.COLOR_BGR2RGB)
 
 
-def get_skeleton_stats(image):
-    skeleton_image = skimage.morphology.skeletonize(image)
+def get_skeleton_stats(binary_image):
+    """
+    Returns stats per branch after skeletonizing the 'binary_image'
+    """
+    skeleton_image = skimage.morphology.skeletonize(binary_image)
     stats_object = skeleton_stats.SkeletonStats(skeleton_image, cutoff=0)
     stats, object_lines = stats_object.get_stats_general(stats_object.networkx_graph)
     return get_aggregate_stats(stats)
 
 
+def dist_between_centroids(centroid1, centroid2):
+    """
+    Returns euclidean distance between two centroids or cartesean coordinates
+    """
+    vect = np.array(centroid1, dtype=np.float64) - np.array(centroid2, dtype=np.float64)
+    return np.linalg.norm(vect)
+
+
 def update_trajectory_path_graph(objects, trajectory_path_graph, frame):
+    """
+    Returns updated trajectory path graph. This function loops through the objects which contain
+    the cell id and centroid. If it doesn't exist, it will initialize them with zeros.
+    For cells appearing in not exactly the first frame - updates them as with the frame specified as beginning frame
+    For other cells updates the trajectory path distances displacement based on the previous trajectory path graph dictionary
+    """
     object_ids = list(trajectory_path_graph.keys())
 
     # if new cells are formed add more empty rows for tracking
     for object_id, centroid in objects.items():
-        # initialize trajectory path graph keys
+        # initialize trajectory path graph keys only for new cell ids
         if object_id not in object_ids:
             trajectory_path_graph[object_id] = dict(
                 beginning_frame=0,
                 ending_frame=0,
-                parent_cell=0)
+                parent_cell=0,
+                distance=0,
+                starting_centroid=centroid,
+                ending_centroid=centroid,
+                displacement=0)
 
     for object_id, centroid in objects.items():
+        # cell already initialized from a previous frame
         if object_id in object_ids:
-            # trajectory_path_graph[object_id]["beginning_frame"] = frame
             trajectory_path_graph[object_id]["ending_frame"] = trajectory_path_graph[object_id]["ending_frame"] + 1
             trajectory_path_graph[object_id]["parent_cell"] = 0
+            previous_centroid = trajectory_path_graph[object_id]["ending_centroid"]
+            trajectory_path_graph[object_id]["ending_centroid"] = centroid
+            trajectory_path_graph[object_id]["distance"] = trajectory_path_graph[object_id]["distance"] + dist_between_centroids(centroid, previous_centroid)
+            trajectory_path_graph[object_id]["displacement"] = dist_between_centroids(centroid, trajectory_path_graph[object_id]["starting_centroid"])
 
         # new cell or a division from parent cell
         else:
             trajectory_path_graph[object_id]["beginning_frame"] = frame
             trajectory_path_graph[object_id]["ending_frame"] = frame + 1
             trajectory_path_graph[object_id]["parent_cell"] = 0
+
     return trajectory_path_graph
 
 
@@ -181,6 +151,8 @@ if __name__ == '__main__':
     tracking_dir = args.tracking_dir
     skeleton_dir = args.skeleton_dir
     annotation_dir = args.annotation_dir
+
+    # list of images in annotation directory
     annotation_files = natsort.natsorted(glob.glob(annotation_dir + "*.png"))
 
     # initialize our centroid tracker and binary_image dimensions
@@ -189,37 +161,39 @@ if __name__ == '__main__':
     shape = binary_image.shape[:2]
     (height, width) = shape[:2]
 
-    # loop over the images in the prediction folder
+    # initialize trajectory path graph
+    trajectory_path_graph = {}
+
+    # loop over the images in the binary annotations folder
     for frame_count, path in enumerate(annotation_files):
         binary_image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
         label_im, _ = scipy.ndimage.label(binary_image)
 
         _, contours, _ = cv2.findContours(binary_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        # obtain our output predictions, and initialize the list of
-        # bounding box rectangles
         centroids = []
-
-        rgb_converted_image = cv2.cvtColor(binary_image, cv2.COLOR_GRAY2RGB)
-        # loop over the detections
         stats_df = []
 
-        for count, contour in enumerate(contours):
+        # Convert to rgb image once to draw rectangle and write cell ids on the binary image
+        rgb_converted_image = cv2.cvtColor(binary_image, cv2.COLOR_GRAY2RGB)
+
+        # loop over each cell's contour one at a time to find its centroid, stats.
+        for contour_count, contour in enumerate(contours):
 
             # Contour stats
-            contour_stats = get_contour_stats(contour)
+            contour_stats = contour_features.get_contour_stats(contour)
             centroids.append(contour_stats["centroid"])
 
             # Skeleton stats
             contour_filled = np.zeros((height, width), dtype=np.uint8)
             contour_filled = cv2.drawContours(contour_filled, [contour], contourIdx=0, color=1, thickness=-1)
 
-            # Drawing rectangle around the contour
+            # Drawing rectangle in green around the contour, and filling the contour with object id
             box = cv2.boundingRect(contour)
             x, y, w, h = box
             rectangle_overlaid_image = cv2.rectangle(rgb_converted_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            rectangle_overlaid_image[contour_filled != 0] = [count, 255, 255]
+            rectangle_overlaid_image[contour_filled != 0] = [contour_count, 255, 255]
 
-            # assert number of objects is 1
+            # assert number of objects is 1, since we are looking one contour at a time
             assert _get_objects(contour_filled) == 1
             skeletonized_arr_stats = get_skeleton_stats(contour_filled)
             contour_stats.update(skeletonized_arr_stats)
@@ -227,15 +201,6 @@ if __name__ == '__main__':
 
         # update our centroid tracker using the computed set of rectangles
         objects = centroid_tracker.update(centroids)
-
-        if frame_count == 0:
-            trajectory_path_graph = {}
-            for object_id in list(objects.keys()):
-                # initialize trajectory path graph keys
-                trajectory_path_graph[object_id] = dict(
-                    beginning_frame=0,
-                    ending_frame=0,
-                    parent_cell=0)
 
         trajectory_path_graph = update_trajectory_path_graph(objects, trajectory_path_graph, frame_count)
 
@@ -251,10 +216,11 @@ if __name__ == '__main__':
                 (0, 255, 0),
                 2)
 
-        # save the output binary_image
+        # save the image with cells tracked and tagged with their IDs and bounding boxes
         save_path = os.path.join(tracking_dir, os.path.basename(path))
         cv2.imwrite(save_path, cv2.cvtColor(rectangle_overlaid_image, cv2.COLOR_BGR2RGB))
 
+        # save the skeleton image, skeleton is overlaid onto the original binary image
         image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
         skeleton_image = skimage.morphology.skeletonize(image // 255)
         overlaid_image = get_overlapped_skeleton_image(image, skeleton_image)
@@ -266,7 +232,6 @@ if __name__ == '__main__':
         df.to_csv(skeleton_dir + os.path.basename(path).replace(".png", "_") + "contour_skeleton_stats.csv")
 
     # Save the tracking path dataframe
-    print(trajectory_path_graph)
     trajectory_df = pd.DataFrame(pd.DataFrame(trajectory_path_graph))
     trajectory_df.to_csv(skeleton_dir + "trajectory_path_graph.csv")
 
